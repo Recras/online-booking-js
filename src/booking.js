bed73ea3b428df7ff8f260a40861e35b3bf5f051
Sander <github@ugoku.nl>
2018-09-25 16:00:18 +0200

    Remove obsolete CSS

--- src/booking.js
+++ src/booking.js
@@ -7,57 +7,6 @@ class RecrasBooking {
     constructor(options = {}) {
         this.datePicker = null;
 
-        const CSS = `
-@import url('https://cdn.rawgit.com/dbushell/Pikaday/eddaaa3b/css/pikaday.css');
-
-.recras-onlinebooking > *:not(.latestError):not(.recrasLoadingIndicator) {
-    padding: 1em 0;
-}
-.recras-onlinebooking > *:not(:first-child) + * {
-    border-top: 2px solid #dedede; /* Any love for Kirby out there? */
-}
-.recras-contactform > div, .recras-amountsform > div {
-    align-items: start;
-    display: flex;
-    justify-content: space-between;
-    padding: 0.25em 0;
-}
-.minimum-amount {
-    padding-left: 0.5em;
-}
-.time-preview {
-    padding-right: 0.5em;
-}
-.minimum-amount {
-    color: hsl(0, 50%, 50%);
-}
-
-.recrasLoadingIndicator {
-    animation: recrasSpinner 1.1s infinite linear;
-    border: 0.2em solid rgba(0, 0, 0, 0.2);
-    border-left-color: rgba(0, 0, 0, 0.5);
-    border-radius: 50%;
-    display: inline-block;
-    height: 2em;
-    overflow: hidden;
-    text-indent: -100vw;
-    width: 2em;
-}
-@keyframes recrasSpinner {
-    0% {
-        transform: rotate(0deg);
-    }
-    100% {
-        transform: rotate(360deg);
-    }
-}
-.bookPackage .recrasLoadingIndicator {
-    height: 1em;
-    margin-left: 0.5em;
-    vertical-align: middle;
-    width: 1em;
-}
-`;
         this.PAYMENT_DIRECT = 'mollie';
         this.PAYMENT_AFTERWARDS = 'factuur';
 
